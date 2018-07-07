714fa261537380900ec844c3a4541281fc0c6da7
fzsun <sunfangzhou1991@gmail.com>
2018-07-07 18:23:08 -0400

    updates

--- example.py
+++ example.py
@@ -7,15 +7,30 @@ Created on Sat Jun 30 21:01:11 2018
 """
 
 from flask import Flask, request
+from waitress import serve
 from gurobipy import *
 import math
 import random
 import itertools
 import os
+import logging
+from user_agents import parse
 
 app = Flask(__name__)
 DEFAULT_N = 20
 n = DEFAULT_N
+
+logger = logging.getLogger(__name__)
+if not logger.hasHandlers():
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler('RunLog.log')
+    formatter = logging.Formatter(
+        fmt='%(asctime)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -31,20 +46,42 @@ def home():
             with open ("gurobi.log", "r") as myfile:
                 mylog=''.join(myfile.readlines())
 
-            result_html = f'''n_cities = {n}</br>
-                        runtime = {runtime:g} sec</br>
-                        Best objective = {objVal:g}</br>
-                        tour = <code>{' -> '.join(map(str,tour))}</code></br>
-                        <hr>
-                        <pre>{mylog}</pre>'''
+            result_html = f'''
+                n_cities = {n}</br>
+                runtime = {runtime:.3f} sec</br>
+                Best objective = {objVal:g}</br>
+                tour = <code>{' -> '.join(map(str,tour))}</code></br>
+                <hr>
+                <pre>{mylog}</pre>'''
         else:
             result_html = f'<p>Please enter number of cities!</p>'
     except ValueError as e:
         result_html = f'ValueError: {str(e)}'
-    form_html = f'''<form>
-          <p>Enter number of cities: <input name="n_cities" value="{n}"></p>
-          <p><button type="submit">Submit</button></p>
-          </form>'''
+    form_html = f'''
+        <form method="get">
+          <fieldset>
+            <legend>User Input:</legend>
+            Enter number of cities:
+              <input type="range" min="3" max="150" name="n_cities" value="{n}" id="myRange">
+              <span id="myValue"></span><br>
+              <button type="submit">Submit</button><br>
+          </fieldset>
+        </form>'''
+    form_html += '''
+        <script>
+            var slider = document.getElementById("myRange");
+            var output = document.getElementById("myValue");
+            output.innerHTML = slider.value;
+
+            slider.oninput = function() {
+              output.innerHTML = this.value;
+            }
+        </script>'''
+    user_agent = str(parse(request.headers.get('User-Agent')))
+    real_ip = str(request.headers.get('X-Real-Ip'))
+    logger.info(f"""cities {'real_ip':^15} user_agent
+                    {n:>6}   {real_ip:^15} {user_agent}
+                    """)
     return form_html + result_html
 
 
@@ -96,7 +133,7 @@ def tsp():
 
     m._vars = vars
     m.Params.lazyConstraints = 1
-#    m.setParam('Threads',6)
+    m.setParam('Threads',6)
 
     m.optimize(subtourelim)
 
@@ -108,9 +145,8 @@ def tsp():
 
 
 if __name__ == '__main__':
-    app.run()
-
-
-
+#    app.run(debug=True)
+    logger.info('================== Begin server ==================')
+    serve(app, listen='*:8080')
 
 
